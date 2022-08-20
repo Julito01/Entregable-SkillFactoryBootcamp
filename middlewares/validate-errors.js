7013900760e8e7e548603307d2a83a14d1c1c741
Julio Dechert <julitod55@hotmail.com>
2022-08-20 20:59:19 -0300

    validate errors

--- middlewares/validate-errors.js
+++ middlewares/validate-errors.js
@@ -1,9 +1,5 @@
-import { validationResult } from 'express-validator';
-
 export const validateErrors = (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json(errors);
-  }
-  next();
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
 };
